9a88f62abcecb819b8c58b1af7f465037031c627
Girllovin <77302722+Girllovin@users.noreply.github.com>
2025-06-02 19:52:44 +0300

    Initial commit

---
+++ src/main/java/smallITgroup/freedive/FreeDiveApplication.java
@@ -0,0 +1,13 @@
+package smallITgroup.freedive;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FreeDiveApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FreeDiveApplication.class, args);
+	}
+
+}
---
+++ src/test/java/smallITgroup/freedive/FreeDiveApplicationTests.java
@@ -0,0 +1,13 @@
+package smallITgroup.freedive;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FreeDiveApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
